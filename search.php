690e7ba184d2c76422248d0b9f1d45bed17da8d1
zachstep05 <46059123+zachstep05@users.noreply.github.com>
2019-01-20 18:51:34 +0000

    Create search.php

---
+++ search.php
@@ -0,0 +1,7 @@
+<?php
+include 'header.php';
+?>
+  <form action="search.php" method="POST">
+ <input type="text" name="search" placeholder="search">
+   <button type="submit" name="submit-search"></button>
+</form>
