29eb88610159ba930709db2f7c8fc3af810ab77f
zachstep05 <46059123+zachstep05@users.noreply.github.com>
2019-01-20 18:51:29 +0000

    Create header.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+include 'dbh.php';
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+   <link rel="stylesheet" type="text/css"href="styles.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="icon-bar">
+  <a class="active" href="index.html"><i class="fa fa-home"></i></a>
+  <a href="#"><i class="fa fa-search"></i></a>
+ <a href="mailto:contactzachstep@gmail.com"><i class="fa fa-envelope"></i></a>
+<a href="https://www.instagram.com/stepzach/"><i class="fa fa-instagram" style="font-size:38px"></i></a>
+  <a href="news.html"><i class="fa fa-user-circle-o"></i></a>
+</div>
